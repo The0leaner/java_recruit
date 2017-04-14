838320cf7c1569ba02c1ad2fca3dd6b70a36cd59
Chen <2641635718@qq.com>
2017-04-14 22:39:44 +0800

    DoubleLinkNodeList & DoubleLinkNode

---
+++ src/main/java/com/chen/DoubleLinkNode.java
@@ -0,0 +1,16 @@
+package com.chen;
+
+
+public  class DoubleLinkNode {
+    public int data;
+    public DoubleLinkNode next;
+    public DoubleLinkNode prev;
+
+    public DoubleLinkNode(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
